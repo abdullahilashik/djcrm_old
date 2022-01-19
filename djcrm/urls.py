c26e8662ebd0b603a0b6699aef69da36671a5872
Ashik Arefin <abdullahilashik@gmail.com>
2022-01-19 08:00:47 +0600

    forget password

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import HomepageView, SignupView
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordResetDoneView, PasswordResetConfirmView,
+    PasswordResetCompleteView, PasswordResetView
+)
 
 urlpatterns = [
     path('', HomepageView.as_view(), name='home'),
@@ -10,5 +13,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('reset-password', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('admin/', admin.site.urls),
 ]
